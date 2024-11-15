f29636c90a958744854c8a3da1824b2154534cd2
kastastin <kastastin.main@gmail.com>
2024-11-15 04:43:04 +0200

    add adaptive >320px (prod: v1)

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,15 @@
+const menu = document.querySelector(".menu");
+const burger = document.querySelector(".burger");
 const menuSelect = document.querySelector(".menu-select");
+const accordionItems = document.querySelectorAll(".accordion__item");
+
+burger.addEventListener("click", () => {
+  menu.classList.toggle("menu--active");
+});
 
 menuSelect.addEventListener("click", () => {
   menuSelect.classList.toggle("menu-select--active");
 });
-
-const testimonialsSlider = new Swiper(".s-testimonials__slider", {
-  loop: true,
-  speed: 800,
-  slidesPerView: 2,
-  slidesPerGroup: 2,
-  spaceBetween: 24,
-  autoplay: {
-    delay: 5000,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    prevEl: ".s-testimonials__btn-prev",
-    nextEl: ".s-testimonials__btn-next",
-  },
-});
-
-// Accordion
-const accordionItems = document.querySelectorAll(".accordion__item");
 
 accordionItems.forEach((item) => {
   item.addEventListener("click", (e) => {
@@ -41,4 +26,29 @@ accordionItems.forEach((item) => {
       clickedItem.classList.add("accordion__item--active");
     }
   });
+});
+
+const testimonialsSlider = new Swiper(".s-testimonials__slider", {
+  loop: true,
+  speed: 800,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  spaceBetween: 24,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".s-testimonials__btn-prev",
+    nextEl: ".s-testimonials__btn-next",
+  },
+  breakpoints: {
+    610: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  },
 });
